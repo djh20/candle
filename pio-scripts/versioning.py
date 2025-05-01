0aeed0fba898395555d5bd469f3f0cac5e0dda6a
djh20 <danmanmrdj@gmail.com>
2025-05-01 22:17:23 +1200

    Update github workflows

--- pio-scripts/versioning.py
+++ pio-scripts/versioning.py
@@ -3,6 +3,6 @@ Import("env")
 version = env["ENV"].get("FIRMWARE_VERSION")
 
 if version is None:
-    version = "0.0.0-d"
+    version = "0.0.0-dev"
 
 env.Append(BUILD_FLAGS=['\'-D FIRMWARE_VERSION="%s"\'' % version])
