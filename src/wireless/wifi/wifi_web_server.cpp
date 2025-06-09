15c3998b795c24ab2efaf2f01e464cb8eab610c4
djh20 <danmanmrdj@gmail.com>
2025-06-09 15:44:14 +1200

    Add ElegantOTA for updating via wifi

--- src/wireless/wifi/wifi_web_server.cpp
+++ src/wireless/wifi/wifi_web_server.cpp
@@ -2,6 +2,7 @@
 #include "../../vehicle/vehicle_manager.h"
 #include "../../metric/metric_manager.h"
 #include <console.h>
+#include <ElegantOTA.h>
 
 JsonDocument WiFiWebServer::doc;
 
@@ -11,6 +12,9 @@ void WiFiWebServer::begin()
   server.on("/api/metrics", HTTP_GET, onMetricsRequest);
   server.on("/api/console", HTTP_POST, onConsoleRequest);
   server.on("/api/vehicle/task", HTTP_POST, onTaskRequest);
+
+  ElegantOTA.begin(&server);
+  ElegantOTA.setAutoReboot(true);
 
   server.begin();
 }
